27c80ded703824ef9706ec7baaa8effea11154cc
JP007-Star <justice_of_peace@yahoo.com>
2022-01-06 12:00:31 +0530

    cart changes updated

---
+++ src/main/java/com/restaurant/app/repository/CartRepository.java
@@ -0,0 +1,18 @@
+/*
+ * @project ResturantApp
+ * @fileName CartRepository
+ * @author Jaya Prasad.M --> jaya_muthukrishnan
+ * @email jaya_muthukrishnan@thbs.com
+ * @date 06 01 2022 10:44 AM
+ */
+package com.restaurant.app.repository;
+
+import com.restaurant.app.model.Cart;
+import com.restaurant.app.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
