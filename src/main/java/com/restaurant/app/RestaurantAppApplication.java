8e27c727a5b67c3590dfab92dbb32a35c7f44bfb
Akshatha Chandraraju <akshatha_chandraraju@thbs.com>
2022-01-06 09:20:03 +0530

    commit product module

--- src/main/java/com/restaurant/app/RestaurantAppApplication.java
+++ src/main/java/com/restaurant/app/RestaurantAppApplication.java
@@ -3,8 +3,6 @@ package com.restaurant.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class RestaurantAppApplication {
 
