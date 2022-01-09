31237fd023386b594d8dc03b34995fb6d432ab96
JP007-Star <justice_of_peace@yahoo.com>
2022-01-09 23:37:53 +0530

    sales chart backend made

--- src/main/java/com/restaurant/app/controller/DashboardController.java
+++ src/main/java/com/restaurant/app/controller/DashboardController.java
@@ -13,16 +13,23 @@ import com.restaurant.app.service.CartService;
 import com.restaurant.app.service.OrderService;
 import com.restaurant.app.service.ProductService;
 import com.restaurant.app.dao.UserServiceImpl;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Controller
-@RequestMapping("/admin/dashboard")
+@RequestMapping("/admin/dashboard/")
 public class DashboardController {
     @Autowired
     ProductService productService;
@@ -33,14 +40,28 @@ public class DashboardController {
     @Autowired
     CartService cartService;
     Double totalRevenue=0.0;
-    @GetMapping
-    public  String index(Model model){
+    @GetMapping("/")
+    public  String index(Model model) throws JSONException {
         long cartCount=cartService.count();
         long productCount=productService.count();
         List<String> productQuantityStockList=productService.findAllQuantity();
         List<String> productNameStockList=productService.findAllProductName();
         long userCount=userService.userRepository.count();
         long orderCount=orderService.count();
+        JSONArray productArray = new JSONArray();
+        JSONArray mJSONArray = new JSONArray();
+
+        for(int value : getThreeValues())
+        {
+            mJSONArray.put(value);
+        }
+        for(String productName : productNameStockList) {
+            JSONObject data= new JSONObject();
+            data.put("name", productName);
+            data.put("data", mJSONArray);
+            productArray.put(data);
+        }
+        System.out.println(productArray);
         model.addAttribute("productCount",productCount);
         model.addAttribute("userCount",userCount);
         model.addAttribute("cartCount",cartCount);
@@ -48,8 +69,14 @@ public class DashboardController {
         model.addAttribute("totalRevenue",revenueCalculator());
         model.addAttribute("productQuantityStockList",productQuantityStockList);
         model.addAttribute("productNameStockList",productNameStockList);
+        model.addAttribute("productArray",productArray);
         return "dashboard";
     }
+    public int[] getThreeValues() {
+        Random r = new Random();
+        return new int[] { r.nextInt(100),r.nextInt(100),r.nextInt(100),r.nextInt(100),r.nextInt(100), r.nextInt(100), r.nextInt(100) };
+    }
+
 
     public Double revenueCalculator(){
         totalRevenue=0.0;
